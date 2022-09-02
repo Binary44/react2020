6f541d4bfb3b5f92b1965e29c7da5430c557823d
Binary44 <binarycoder.com@gmail.com>
2022-09-02 15:29:20 +0100

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,29 @@
 import React from 'react'
-import { Route,Routes } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import Logo from "../images/logo.svg"
+import './Navbar.css'
 
 
 const Navbar = () => {
   return (
-    <div>
-        
+    <div className='container'>
+        <ul>
+            <div className='nav1'>
+            <NavLink to="/">
+                <img src={Logo} alt="logo" />
+            </NavLink>
+            </div>
+            <div className='nav2'>
+            <li><NavLink to="features">Features</NavLink></li>
+            <li><NavLink to="company">Company</NavLink></li>
+            <li><NavLink to="carees">Carees</NavLink></li>
+            <li><NavLink to="about">About</NavLink></li>
+            </div>
+            <div className='nav3'>
+            <li><NavLink to="login">Login</NavLink></li>
+            <li><NavLink  to="register">Register</NavLink></li>
+            </div>
+        </ul>
     </div>
   )
 }
