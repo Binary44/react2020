2bfa34f1112012ee47a70e24c2a6c6214e40477c
Binary44 <binarycoder.com@gmail.com>
2022-09-02 17:14:10 +0100

    hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import hero from '../images/heroimg.png'
+import './Hero.css'
 
 const Hero = () => {
   return (
     <div className='container'>
         <div className='content'>
-            <h1>make Remote <br /> Work</h1>
+            <h1>make <br /> Remote  Work</h1>
             <p>Get your team in sync, no matter your location. <br /> Streamline processs, create team rituals, and <br />watch producivity solar.</p>
             <button>Learn More</button>
         </div>
-        <div className='image'>
-            <img src={hero} alt="" />
+        <div>
+            <img className='image' src={hero} alt="" />
         </div>
     </div>
   )
