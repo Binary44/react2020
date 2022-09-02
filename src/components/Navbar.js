df0760067b003c1107b24c6bb36c5c5ee3e84592
Binary44 <binarycoder.com@gmail.com>
2022-09-02 14:44:08 +0100

    cleanup

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Route,Routes } from 'react-router-dom';
+
+
+const Navbar = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Navbar
